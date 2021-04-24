6319a46e3ec67b40b117c664040acd95345d90c0
Jim Magnusson <jim.magnusson97@gmail.com>
2021-04-24 11:35:45 +0200

    Implemented basic jumping

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    [SerializeField] float jumpSpeed = 2f;
+
+    private bool canJump = false;
+    private Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 jumpVelocityToAdd = new Vector3(0f, jumpSpeed, 0f);
+            rigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+}
