14cdc66fb67eefe970814cceb7a7636dd9200954
Jim Magnusson <jim.magnusson97@gmail.com>
2021-04-24 09:50:45 +0200

    Added pillar and simple rotation script.

---
+++ Assets/Scripts/RotateOnInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnInput : MonoBehaviour
+{
+
+    [SerializeField] [Tooltip("Degrees/s")] float rotationSpeed = 45f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime);
+    }
+}
