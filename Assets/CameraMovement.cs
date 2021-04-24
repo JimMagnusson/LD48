e8fefb6ac31710d62abaaf093a1feb87e83d86ff
Jim Magnusson <jim.magnusson97@gmail.com>
2021-04-24 18:18:33 +0200

    Fixed camera bug

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -15,6 +15,9 @@ public class CameraMovement : MonoBehaviour
 
     private int currentSegmentIndex = 1;
 
+    private bool isMoving = true;
+    private const float DOLLY_AT_END_DIFF = 0.1f;
+
     /*
     * Adds a waypoint to the DollyTrack with SEGMENT_LENGTH below the last one (y-axis).
     * Ex. Waypoints = [(0,0,0)] => Waypoints = [(0,0,0),(0, -SEGMENT_LENGTH, 0)]
@@ -44,6 +47,31 @@ public class CameraMovement : MonoBehaviour
 
     void Update()
     {
-        dolly.m_PathPosition += cameraSpeed * Time.deltaTime;
+        if(isMoving == true)
+        {
+            dolly.m_PathPosition += cameraSpeed * Time.deltaTime;
+        }
+        CheckMovement(); // Perhaps unneccessary since the game should end when player hits the top frame of the camera.
+
+        //float cameraPosition = dolly.m_Path
+        //Debug.Log("Dolly position: " + dolly.m_PathPosition);
+        //Debug.Log("Convert pos: " + dolly.m_PositionUnits);
+    }
+
+    /*
+     * Stops the movement if the dolly has reached its 
+     */
+    private void CheckMovement()
+    {
+        CinemachineSmoothPath currentPath = (CinemachineSmoothPath)dolly.m_Path;
+        float diff = Mathf.Abs(dolly.m_PathPosition - (currentPath.m_Waypoints.Length - 1));
+        if (diff <= DOLLY_AT_END_DIFF)
+        {
+            isMoving = false;
+        }
+        else
+        {
+            isMoving = true;
+        }
     }
 }
