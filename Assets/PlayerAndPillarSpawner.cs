71bf78359832403273f887bec90ce83378d1cd0b
Jim Magnusson <jim.magnusson97@gmail.com>
2021-04-25 14:56:41 +0200

    PIllar spawning and collision changes.

---
+++ Assets/PlayerAndPillarSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAndPillarSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject playerPrefab;
+    [SerializeField] GameObject pillarPrefab;
+    [SerializeField] GameObject cylinderPrefab;
+
+
+    [SerializeField] Vector3 playerStartPos;
+    [SerializeField] int maxPillarCount = 4;
+    [SerializeField] int numberOfStartingCylinders = 3;
+
+    private GameObject newPillar = null;
+
+    private Queue<GameObject> pillars;
+    private float pillarYPos = 0;
+    private int numberOfPillarsSpawned = 0;
+    private const float PILLAR_LENGTH = 23.937f;
+
+    void Start()
+    {
+        pillars = new Queue<GameObject>();
+        SpawnStartingCylinders();
+        SpawnNextPillar();
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        Instantiate(playerPrefab, playerStartPos, Quaternion.identity);
+    }
+
+    private void SpawnStartingCylinders()
+    {
+        for(int i = numberOfStartingCylinders; i > 0; i--)
+        {
+            GameObject cylinder = Instantiate(cylinderPrefab, new Vector3(0, i * PILLAR_LENGTH, 0), Quaternion.identity,transform);
+            pillars.Enqueue(cylinder);
+        }
+    }
+
+    public void SpawnNextPillar()
+    {
+        pillarYPos = PILLAR_LENGTH * numberOfPillarsSpawned;
+
+        newPillar = Instantiate(pillarPrefab, new Vector3(0, -pillarYPos,0), Quaternion.identity);
+        newPillar.GetComponentInChildren<TopColliderHandler>().PillarSpawner = this;
+        newPillar.GetComponentInChildren<BottomColliderHandler>().PillarSpawner = this;
+        pillars.Enqueue(newPillar);
+        numberOfPillarsSpawned++;
+
+        if(pillars.Count > maxPillarCount)
+        {
+            GameObject pillarToRemove = pillars.Dequeue();
+            Destroy(pillarToRemove);
+        }
+    }
+}
---
+++ Assets/TopColliderHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopColliderHandler : MonoBehaviour
+{
+    public PlayerAndPillarSpawner PillarSpawner { get; set; }
+    private bool isTriggered = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if (isTriggered) { return; }
+            isTriggered = true;
+
+            PillarSpawner.SpawnNextPillar();
+        }
+    }
+}
